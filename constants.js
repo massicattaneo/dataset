eab65d4289a1d65cf316717d2f139ca4d7bc5ab2
massicattaneo <massi.cattaneo.it@gmail.com>
2022-02-13 11:29:40 +0100

    Last commit for the name

--- constants.js
+++ constants.js
@@ -1,69 +1,69 @@
 const APPLICATION = {
-    NAME: 'Live',
-    VERSION: process.env.PRODUCT_VERSION
+    NAME: "Dataset",
+    VERSION: process.env.PRODUCT_VERSION,
 };
 
 const PORT = 8095;
-const HOSTNAME = '192.168.0.53';
+const HOSTNAME = "192.168.0.76";
 // const HOSTNAME = 'localhost';
-const HOST = `${HOSTNAME}${PORT ? `:${PORT}` : ''}`;
+const HOST = `${HOSTNAME}${PORT ? `:${PORT}` : ""}`;
 const SERVER = {
-    ROOT: '/',
+    ROOT: "/",
     PORT,
     WSS_ORIGIN: `ws://${HOST}`,
-    HTTPS_ORIGIN: `http://${HOST}`
+    HTTPS_ORIGIN: `http://${HOST}`,
 };
 
 const STYLE = {
-    WHITE_COLOR: '#ffffff',
-    MAIN_COLOR: '#80aec4',
-    GREEN_COLOR: '#58be55',
-    CONTRAST_COLOR: '#3e3e3e',
-    ACCENT_COLOR: '#dca34b',
-    SOFT_GRAY_COLOR: '#d4d1d1',
-    TEXT_COLOR: '#5a616d',
-    TEXT_BG_COLOR: '#e5e5e5',
-    FONT_FAMILY: 'arialnarrow',
+    WHITE_COLOR: "#ffffff",
+    MAIN_COLOR: "#80aec4",
+    GREEN_COLOR: "#58be55",
+    CONTRAST_COLOR: "#3e3e3e",
+    ACCENT_COLOR: "#dca34b",
+    SOFT_GRAY_COLOR: "#d4d1d1",
+    TEXT_COLOR: "#5a616d",
+    TEXT_BG_COLOR: "#e5e5e5",
+    FONT_FAMILY: "arialnarrow",
     HOME_HEADER_HEIGHT: 29,
-    HOME_FOOTER_HEIGHT: 60
+    HOME_FOOTER_HEIGHT: 60,
 };
 
-const API_ROOT = '/api';
+const API_ROOT = "/api";
 const API = {
     ROOT: API_ROOT,
     ACCOUNT: {
         STATUS: `${API_ROOT}/account/status`,
         REGISTER: `${API_ROOT}/account/register`,
         LOGIN: `${API_ROOT}/account/login`,
-        EXISTS: `${API_ROOT}/account/exists`
+        EXISTS: `${API_ROOT}/account/exists`,
     },
     REST: {
-        GET: '/api/rest/:table'
-    }
+        GET: "/api/rest/:table",
+    },
 };
 
 const HTTP_STATUSES = {
     OK: 200,
     CREATED: 201,
     INVALID_VALIDATION: 422,
-    UNAUTHORIZED: 401
+    UNAUTHORIZED: 401,
 };
 
 const DB = {
     TABLES: {
-        ACCOUNTS: 'accounts'
-    }
+        ACCOUNTS: "accounts",
+    },
 };
 
 const WS = {
     MESSAGES: {
-        CONNECTED: 'connected',
-        HEARTBEAT: 'heartbeat'
-    }
+        CONNECTED: "connected",
+        HEARTBEAT: "heartbeat",
+    },
 };
 
-const ROUTES_PATH = 'routes/';
-const DEFAULT_LANGUAGE = 'en';
+const ROUTES_PATH = "routes/";
+const DEFAULT_LANGUAGE = "en";
 
 module.exports = {
     APPLICATION,
@@ -74,5 +74,5 @@ module.exports = {
     DEFAULT_LANGUAGE,
     HTTP_STATUSES,
     DB,
-    WS
+    WS,
 };
